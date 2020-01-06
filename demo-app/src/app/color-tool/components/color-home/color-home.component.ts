7b06c73ed9ef79059fdb127cdb23e1734d66c194
Eric Greene <eric@t4d.io>
2020-01-06 11:35:28 -0500

    add code demos

--- demo-app/src/app/color-tool/components/color-home/color-home.component.ts
+++ demo-app/src/app/color-tool/components/color-home/color-home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+import { Color } from '../../models/color';
 
 @Component({
   selector: 'app-color-home',
@@ -9,9 +12,40 @@ export class ColorHomeComponent implements OnInit {
 
   headerText = 'Color Tool';
 
-  constructor() { }
+  colors: Color[] = [
+    { id: 1, name: 'red' },
+    { id: 2, name: 'blue' },
+    { id: 3, name: 'gray' },
+    { id: 4, name: 'purple' },
+  ];
+
+  colorForm: FormGroup;
+
+  // private formBuilder: FormBuilder;
+
+  // constructor(formBuilder: FormBuilder) {
+  //   this.formBuilder = formBuilder;
+  // }
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+
+    this.colorForm = this.formBuilder.group({
+      colorName: '',
+      // add additional form fields here...
+    });
+  }
+
+  addColor() {
+
+    this.colors = this.colors.concat({
+      name: this.colorForm.value.colorName,
+      id: Math.max(...this.colors.map(c => c.id), 0) + 1,
+    });
+
+    this.colorForm.reset();
+
   }
 
 }
